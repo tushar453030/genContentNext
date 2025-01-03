3553c2e235df0666f11753c50efebcd8842cd2db
Tushar Sharma <tushar4530@gmail.com>
2025-01-03 16:54:39 +0530

    youtube transcript API change

--- app/(api)/Blog/route.js
+++ app/(api)/Blog/route.js
@@ -1,4 +1,5 @@
 import { geminiModel, getYouTubeTranscript } from '@/lib/utils'
+import axios from 'axios'
 
 export async function GET(request) {
   if (request.method === 'OPTIONS') {
@@ -13,14 +14,39 @@ export async function GET(request) {
   const searchParams = request.nextUrl.searchParams
   const url = searchParams.get('url')
 
-  const transcript = await getYouTubeTranscript(url)
+  const options = {
+    method: 'POST',
+    url: 'https://video-transcript-scraper.p.rapidapi.com/',
+    headers: {
+      'x-rapidapi-key': process.env.SCRAPE_API_KEY,
+      'x-rapidapi-host': 'video-transcript-scraper.p.rapidapi.com',
+      'Content-Type': 'application/json',
+    },
+    data: {
+      video_url: url,
+      language: 'en',
+    },
+  }
+
+  let response
+  let transcriptString
+  try {
+    response = await axios.request(options)
+    console.log(response.data)
+    const transcriptArray = response.data.transcript // Assuming this is the array
+    transcriptString = transcriptArray.map((item) => item.text).join('\n')
+    transcriptString +=
+      'Include this Video Url in your response at the end' + url + '/n'
+  } catch (error) {
+    console.error(error)
+  }
 
   const prompt = `
-    Act as a user who created a video on youtube now create a blog on it. The word limit must be below 500. Use the below transcript to know the content about the video and then frame your response:
-    Transcript: ${transcript}
+    Act as a user who created a video on youtube now create a blog on it and don't include placeholder in the response. The word limit must be below 500. Use the below transcript to know the content about the video and then frame your response:
+    Transcript: ${transcriptString}
     `
   const result = await geminiModel.generateContent(prompt)
-  const response = result.response.text()
+  response = result.response.text()
   console.log(response)
 
   return new Response(JSON.stringify({ response: response }), {
--- app/(api)/Tweet/route.js
+++ app/(api)/Tweet/route.js
@@ -1,4 +1,5 @@
 import { geminiModel, getYouTubeTranscript } from '@/lib/utils'
+import axios from 'axios'
 
 export async function GET(request) {
   if (request.method === 'OPTIONS') {
@@ -14,14 +15,39 @@ export async function GET(request) {
   const url = searchParams.get('url')
   console.log(url)
 
-  const transcript = await getYouTubeTranscript(url)
+  const options = {
+    method: 'POST',
+    url: 'https://video-transcript-scraper.p.rapidapi.com/',
+    headers: {
+      'x-rapidapi-key': process.env.SCRAPE_API_KEY,
+      'x-rapidapi-host': 'video-transcript-scraper.p.rapidapi.com',
+      'Content-Type': 'application/json',
+    },
+    data: {
+      video_url: url,
+      language: 'en',
+    },
+  }
+
+  let response
+  let transcriptString
+  try {
+    response = await axios.request(options)
+    console.log(response.data)
+    const transcriptArray = response.data.transcript // Assuming this is the array
+    transcriptString = transcriptArray.map((item) => item.text).join('\n')
+    transcriptString +=
+      'Include this Video Url in your response at the end' + url + '/n'
+  } catch (error) {
+    console.error(error)
+  }
 
   const prompt = `
-    Act as a user who created a video on youtube now create a tweet for Twitter. The character limit must be below 200 also add necessary hashtags at the end of the post. Use the below transcript to frame your response:
-    Transcript: ${transcript}
+    Act as a user who created a video on youtube now create a tweet for Twitter. The character limit must be below 200 also add necessary hashtags at the end of the post and don't include placeholder in the response. Use the below transcript regarding the video to frame your response:
+    Transcript: ${transcriptString}.
     `
   const result = await geminiModel.generateContent(prompt)
-  const response = result.response.text()
+  response = result.response.text()
   console.log(response)
 
   return new Response(JSON.stringify({ response: response }), {
